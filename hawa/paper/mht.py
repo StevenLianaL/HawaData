3405e1d54d963cfbe313fbd82df5b84178007626
wz <wangzhou8284@gmail.com>
2022-12-01 20:51:28 +0800

    count 参测学生总量表得分图

--- hawa/paper/mht.py
+++ hawa/paper/mht.py
@@ -10,4 +10,17 @@ class MhtData(CommonData):
     code_word_list: Set[str] = field(default_factory=lambda: {'mht'})
 
     def _to_count_c_student_score(self):
-        """学生总量表得分图数据，横轴分数，纵轴分数"""
+        """学生总量表得分图数据，横轴分数，纵轴人数"""
+        data = []
+        for score, row in self.final_scores.groupby('score'):
+            data.append((int(score), row.score.count()))
+        data.sort(key=lambda x: x[0])
+        scores, people_count = [], []
+        for (score, student_count) in data:
+            scores.append(score)
+            people_count.append(student_count)
+        return {
+            "name": f"{self.meta_unit.name}参测学生总量表得分图",
+            "scores": scores,
+            "counts": people_count
+        }
