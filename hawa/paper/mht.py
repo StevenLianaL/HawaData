99a1c0268010b3c666ffc833392bc8b2ad5956fa
wz <wangzhou8284@gmail.com>
2022-12-01 21:25:31 +0800

    count 年级 参测学生总量表得分图

--- hawa/paper/mht.py
+++ hawa/paper/mht.py
@@ -2,6 +2,8 @@ from collections import defaultdict
 from dataclasses import dataclass, field
 from typing import Set
 
+import pandas as pd
+
 from hawa.common.data import CommonData
 
 
@@ -11,20 +13,8 @@ class MhtData(CommonData):
     code_word_list: Set[str] = field(default_factory=lambda: {'mht'})
 
     def _to_count_c_student_score(self):
-        """学生总量表得分图数据，横轴分数，纵轴人数"""
-        data = []
-        for score, row in self.final_scores.groupby('score'):
-            data.append((int(score), row.score.count()))
-        data.sort(key=lambda x: x[0])
-        x_axis, y_axis = [], []
-        for (score, student_count) in data:
-            x_axis.append(score)
-            y_axis.append(student_count)
-        return {
-            "name": f"{self.meta_unit.name}参测学生总量表得分图",
-            "x_scores": x_axis,
-            "y_counts": y_axis
-        }
+        """学生总量表得分图数据，横轴分数，纵轴人数 （总 及 各年级）"""
+        return self._tool_count_student_score(score=self.final_scores)
 
     def _to_count_d_sub_code_score(self):
         """在 8 个子量表上的得分图，横轴量表，纵轴分数"""
@@ -40,4 +30,28 @@ class MhtData(CommonData):
             "name": f"{self.meta_unit.name}参测学生在 8 个子量表上的得分图",
             "x_mht": x_axis,
             "y_count": y_axis,
+        }
+
+    def _to_count_e_grade_student_score(self):
+        """参考 _to_count_c_student_score， 分年级计算"""
+        res = {}
+        for grade, grade_group in self.final_scores.groupby(by='grade'):
+            grade_data = self._tool_count_student_score(score=grade_group)
+            res[grade] = grade_data
+        return res
+
+    # 计算工具
+    def _tool_count_student_score(self, score: pd.DataFrame):
+        data = []
+        for score, row in score.groupby('score'):
+            data.append((int(score), row.score.count()))
+        data.sort(key=lambda x: x[0])
+        x_axis, y_axis = [], []
+        for (score, student_count) in data:
+            x_axis.append(score)
+            y_axis.append(student_count)
+        return {
+            "name": f"{self.meta_unit.name}参测学生总量表得分图",
+            "x_scores": x_axis,
+            "y_counts": y_axis
         }
